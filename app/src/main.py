32d6d4ee6a8ec952f92ec3f4e1a2b878906b9b57
KKWANH <kwanho0096@gmail.com>
2023-11-06 15:04:05 +0000

    [dev] tutorials - get speed example

--- app/src/main.py
+++ app/src/main.py
@@ -78,7 +78,7 @@ class SampleApp(VehicleApp):
         # - Publishes current speed to MQTT Topic (i.e. DATABROKER_SUBSCRIPTION_TOPIC).
         await self.publish_event(
             DATABROKER_SUBSCRIPTION_TOPIC,
-            json.dumps({"speed": vehicle_speed}),
+            json.dumps({"speed!": vehicle_speed}),
         )
 
     @subscribe_topic(GET_SPEED_REQUEST_TOPIC)
@@ -106,7 +106,7 @@ class SampleApp(VehicleApp):
                 {
                     "result": {
                         "status": 0,
-                        "message": f"""Current Speed = {vehicle_speed}""",
+                        "message": f"""Current !Speed = {vehicle_speed}""",
                     },
                 }
             ),
